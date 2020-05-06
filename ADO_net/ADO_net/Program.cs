c7ee3e8297c6291b1b4a5ac830fecd3a7de98b97
Muzhyk Vitalii Admin <vmuzhadm@internal.adelina.com.ua>
2020-05-06 21:46:36 +0300

    Create Solution

---
+++ ADO_net/ADO_net/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ADO_net
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
