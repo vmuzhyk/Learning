1bb3b5a1ec4bd1c2cc2c33a1f0b6ac99b5ef939b
Muzhyk Vitalii Admin <vmuzhadm@internal.adelina.com.ua>
2020-05-06 22:27:22 +0300

    Create Table Girls

--- ADO_net/ADO_net/Program.cs
+++ ADO_net/ADO_net/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Data.SqlClient;
 
 namespace ADO_net
 {
@@ -6,7 +7,34 @@ namespace ADO_net
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            SqlConnection con = new SqlConnection(@"Data Source=.\SQLEXPRESS;Initial Catalog=Life;Integrated Security=True");
+            string query =
+            @"CREATE TABLE dbo.Girls
+                (
+                    ID int IDENTITY(1,1) NOT NULL,
+                    Name nvarchar(50) NULL,
+                    eye_color nvarchar(50) NULL,
+                    age int NOT NULL,
+                    is_available bit NOT NULL,
+                    first_date date NULL,
+                    CONSTRAINT pk_id PRIMARY KEY (ID)
+                );";
+            SqlCommand cmd = new SqlCommand(query, con);
+            try
+            {
+                con.Open();
+                cmd.ExecuteNonQuery();
+                Console.WriteLine("Table Created Successfully");
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine("Error Generated. Details: " + e.ToString());
+            }
+            finally
+            {
+                con.Close();
+                Console.ReadKey();
+            }
         }
     }
 }
