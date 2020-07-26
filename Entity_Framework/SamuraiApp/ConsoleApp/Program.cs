e678a3226946c8a6152c57ed80c187665a82c859
vmuzhyk <vmuzhyk@outlook.com>
2020-07-26 15:54:17 +0300

    Create dbContext and execute query

--- Entity_Framework/SamuraiApp/ConsoleApp/Program.cs
+++ Entity_Framework/SamuraiApp/ConsoleApp/Program.cs
@@ -1,12 +1,38 @@
 ﻿using System;
+using System.Linq;
+using SamuraiApp.Data;
+using SamuraiApp.Domain;
 
 namespace ConsoleApp
 {
     class Program
     {
-        static void Main(string[] args)
+        private static SamuraiContext context = new SamuraiContext(); 
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            context.Database.EnsureCreated();
+            GetSamurais("Before Add:");
+            AddSamurai();
+            GetSamurais("After Add:");
+            Console.Write("Press any key...");
+            Console.ReadKey();
+        }
+
+        private static void AddSamurai()
+        {
+            var samurai = new Samurai{Name = "Sampson"};
+            context.Samurais.Add(samurai);
+            context.SaveChanges();
+        }
+
+        private static void GetSamurais(string text) 
+        {
+            var samurais = context.Samurais.ToList();
+            Console.WriteLine($"{text}: Samurai count is {samurais.Count}");
+            foreach(var samurai in samurais)
+            {
+                Console.WriteLine(samurai.Name);
+            }
         }
     }
 }
