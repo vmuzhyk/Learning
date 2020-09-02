1686883f7de68b4f7400bb929d200f359d0f2187
vmuzhyk <vmuzhyk@outlook.com>
2020-09-02 22:52:48 +0300

    Create Context and Methods that make changes in DB

--- Entity_Framework/SamuraiApp/Program/Program.cs
+++ Entity_Framework/SamuraiApp/Program/Program.cs
@@ -1,12 +1,37 @@
 ﻿using System;
+using System.Linq;
+using SamuraiApp.Data;
+using SamuraiApp.Domain;
 
 namespace Program
 {
     class Program
     {
+        private static SamuraiContext context = new SamuraiContext();
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            context.Database.EnsureCreated();
+            GetSamurais("Before Add:");
+            AddSamurai();
+            GetSamurais("After Add:");
+            Console.Write("Press any key...");
+            Console.ReadKey();
+        }
+        private static void AddSamurai()
+        {
+            var samurai = new Samurai { Name = "Sampson"};
+            context.Samurais.Add(samurai);
+            context.SaveChanges();
+        }
+
+        private static void GetSamurais(string text)
+        {
+            var samurais = context.Samurais.ToList();
+            Console.WriteLine($"{text}: Samurai count is {samurais.Count}");
+            foreach(var samurai in samurais)
+            {
+                Console.WriteLine(samurai.Name);
+            }
         }
     }
 }
--- Entity_Framework/SamuraiApp/SamuraiApp.Domain/Samurai.cs
+++ Entity_Framework/SamuraiApp/SamuraiApp.Domain/Samurai.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+
 namespace SamuraiApp.Domain
 {
     public class Samurai
