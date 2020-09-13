9a12c5bc291372f4ba2271c9adc1ed769c9c418c
vmuzhyk <vmuzhyk@outlook.com>
2020-09-13 23:51:48 +0300

    Creating another migration

--- Entity_Framework/SamuraiApp/SamuraiApp.Data/SamuraiContext.cs
+++ Entity_Framework/SamuraiApp/SamuraiApp.Data/SamuraiContext.cs
@@ -8,6 +8,7 @@ namespace SamuraiApp.Data
         public DbSet<Samurai> Samurais { get; set; }
         public DbSet<Quote> Quotes { get; set; }
         public DbSet<Clan> Clans { get; set; }
+        public DbSet<Battle> Battles { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer(
@@ -17,6 +18,7 @@ namespace SamuraiApp.Data
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<SamuraiBattle>().HasKey(s => new{s.SamuraiId, s.BattleId});
+            modelBuilder.Entity<Horse>().ToTable("Horses");
         }
     }
 }
