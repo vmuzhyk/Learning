0cc99c0168a2c16610d60e68c86f596b713babea
vmuzhyk <vmuzhyk@outlook.com>
2020-09-24 23:49:54 +0300

    Persisting Data in disconected scenarios

--- Entity_Framework/SamuraiApp/Program/Program.cs
+++ Entity_Framework/SamuraiApp/Program/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using Microsoft.EntityFrameworkCore;
 using SamuraiApp.Data;
 using SamuraiApp.Domain;
 
@@ -7,33 +8,90 @@ namespace Program
 {
     class Program
     {
-        private static SamuraiContext context = new SamuraiContext();
+        private static SamuraiContext _context = new SamuraiContext();
         static void Main(string[] args)
         {
-            context.Database.EnsureCreated();
+            //_context.Database.EnsureCreated();
             // GetSamurais("Before Add:");
             // GetSamurais("After Add:");
-            InsertMultipleSamurais();
-            Console.Write("Press any key...");
+             //InsertMultipleSamurais();
+            // Console.Write("Press any key...");
+            //QueryFilters();
+            //RetreiveAndUpdateSamurai();
+            //RetreiveAndUpdateMultipleSamurais();
+            //MultipleDatabaseOperation();
+            InsertBattle();
+            QueryAndUpdateBattleDisconected();
+            
             Console.ReadKey();
         }
         private static void InsertMultipleSamurais()
         {
-            var samurai = new Samurai { Name = "Jasper"};
-            var samurai2 = new Samurai { Name = "Garlem"};
-            var samurai3 = new Samurai { Name = "Mika"};
-            var samurai4 = new Samurai { Name = "Kirby"};
-            context.Samurais.AddRange(samurai, samurai2, samurai3, samurai4);
-            context.SaveChanges();
+            var samurai = new Samurai { Name = "Liam"};
+            var samurai2 = new Samurai { Name = "Wisdom"};
+            var samurai3 = new Samurai { Name = "Poppy"};
+            var samurai4 = new Samurai { Name = "Malcolm"};
+            _context.Samurais.AddRange(samurai, samurai2, samurai3, samurai4);
+            _context.SaveChanges();
         }
 
         private static void GetSamurais(string text)
         {
-            var samurais = context.Samurais.ToList();
+            var samurais = _context.Samurais.ToList();
             Console.WriteLine($"{text}: Samurai count is {samurais.Count}");
             foreach(var samurai in samurais)
             {
                 Console.WriteLine(samurai.Name);
+            }
+        }
+
+        private static void QueryFilters() 
+        {
+            var name = "Mika";
+            var samurais = _context.Samurais.Where(s => s.Name == name).ToList();
+        }
+
+        private static void RetreiveAndUpdateSamurai()
+        {
+            var samurai = _context.Samurais.FirstOrDefault();
+            samurai.Name += "San";
+            _context.SaveChanges();
+        }
+
+        private static void RetreiveAndUpdateMultipleSamurais()
+        {
+            var samurais = _context.Samurais.Skip(3).Take(4).ToList();
+            samurais.ForEach(s => s.Name += "San");
+            _context.SaveChanges();
+        }
+
+        private static void MultipleDatabaseOperation()
+        {
+            var samurai = _context.Samurais.FirstOrDefault();
+            samurai.Name += "San";
+            _context.Samurais.Add(new Samurai{Name = "Kilon"});
+            _context.SaveChanges();
+        }
+
+        private static void InsertBattle()
+        {
+            _context.Battles.Add( new Battle
+            {
+                Name = "Battle of Okehazama",
+                StartDate = new DateTime(1560, 05, 01),
+                EndDate = new DateTime(1560, 06, 15)
+            });
+            _context.SaveChanges();
+        }
+
+        private static void QueryAndUpdateBattleDisconected()
+        {
+            var battle = _context.Battles.AsNoTracking().FirstOrDefault();
+            battle.EndDate = new DateTime(1560, 06, 30);
+            using (var newContextInstance = new SamuraiContext())
+            {
+                newContextInstance.Battles.Update(battle);
+                newContextInstance.SaveChanges();
             }
         }
     }
