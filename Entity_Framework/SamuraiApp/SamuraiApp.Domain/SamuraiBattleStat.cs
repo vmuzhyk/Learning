7a26125cb9754279fb4f852c0969687d067f9b42
vmuzhyk <vmuzhyk@outlook.com>
2020-10-15 23:32:38 +0300

    Using keyless Entities to mapping to views

---
+++ Entity_Framework/SamuraiApp/SamuraiApp.Domain/SamuraiBattleStat.cs
@@ -0,0 +1,9 @@
+namespace SamuraiApp.Data
+{
+    public partial class SamuraiBattleStat
+    {
+        public string Name { get; set; }
+        public int? NumberOfBattles { get; set; }
+        public string EarliestBattle { get; set; }
+    }
+}
