803bbf95543d8b39a680708a67b2dabecd849734
vmuzhyk <vmuzhyk@outlook.com>
2020-10-23 23:39:38 +0300

    Create ASP.NET project and add SamuraisController

---
+++ Entity_Framework/SamuraiApp_v2/SamuraiApp.Data/Migrations/20201017082605_CreateNewSproc.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SamuraiApp.Data.Migrations
+{
+    public partial class CreateNewSproc : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                @"CREATE PROCEDURE dbo.SamuraisWhoSaidWord
+                    @text VARCHAR(20)
+                    AS
+                    SELECT  Samurais.Id, Samurais.Name, Samurais.ClanId
+                    FROM    Samurais INNER JOIN
+                            Quotes ON Samurais.Id = Quotes.SamuraiId
+                    WHERE   (Quotes.Text LIKE '%'+@text+'%')");
+
+            migrationBuilder.Sql(
+                @"CREATE PROCEDURE dbo.DeleteQuotesForSamurai
+                    @samuraiId int
+                    AS DELETE FROM Quotes
+                    WHERE Quotes.SamuraiId=@samuraiId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
